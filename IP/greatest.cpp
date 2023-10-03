ede0804847d479b7c2cf6c130c4dac7c8e965402
Sadman Sakib <sakibsadman035@gmail.com>
2023-10-04 02:12:04 +0600

    greatest number using nested if-else

---
+++ IP/greatest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter 3 numbers: ";
+    cin >> a >> b >> c;
+
+    if (a > b) {
+        if (a > c) {
+            cout << a << " is greatest";
+        }
+        else {
+            cout << c << " is greatest";
+        }
+    }
+    else {
+        if (b > c) {
+            cout << b << " is greatest";
+        }
+        else {
+            cout << c << " is greatest";
+        }
+    }
+}
