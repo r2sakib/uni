093353d9b3aa7d464e03c245204da8fdded4f078
Sadman Sakib <sakibsadman035@gmail.com>
2024-02-11 23:17:08 +0600

    Task 2

--- OOP1/Lab_Task-2/Mobile/Mobile.java
+++ OOP1/Lab_Task-2/Mobile/Mobile.java
@@ -1,15 +1,15 @@
 public class Mobile {
     private String mobileOwnerName;
     private String mobileNumber;
-    private String mobileBalance;
+    private double mobileBalance;
     private String mobileOSName;
     private boolean lock;
 
-    public Mobile (String mobileOwnerName, String mobileNumber, String mobileBanalce, String mobileOSName, boolean lock) {
+    public Mobile (String mobileOwnerName, String mobileNumber, double mobileBalance, String mobileOSName, boolean lock) {
         setMobileOwnerName(mobileOwnerName);
         setMobileNumber(mobileNumber);
         setMobileBalance(mobileBalance);
-        setmobileOSName(mobileOSName);
+        setMobileOSName(mobileOSName);
         setLock(lock);
     }
 
@@ -23,7 +23,77 @@ public class Mobile {
         this.mobileNumber = mobileNumber;
     }
 
-    public void 
+    public void setMobileBalance(double mobileBalance) {
+        this.mobileBalance = mobileBalance;
+    }
+
+    public void setMobileOSName(String mobileOSName) {
+        this.mobileOSName = mobileOSName;
+    }
+
+    public void setLock(boolean lock) {
+        this.lock = lock;
+    }  
+
+    public String getMobileOwnerName() {
+        return mobileOwnerName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public double getMobileBalance() {
+        return mobileBalance;
+    }
+
+    public String getMobileOSName() {
+        return mobileOSName;
+    }
+
+    public void showDetails() {
+        if (lock == false) {
+            System.out.println("----------------------------");
+            System.out.println("Mobile Owner Name: " + mobileOwnerName);
+            System.out.println("Mobile Number: " + mobileNumber);
+            System.out.println("Mobile balance: " + mobileBalance);
+            System.out.println("Mobile OS Name: " + mobileOSName);
+            System.out.println("----------------------------");
+        }
+        else {
+            System.out.println("Phone is locked.");
+        }
+    }
+
+    public void recharge(int amount) {
+        if (lock == false) {
+            mobileBalance += amount;
+            System.out.println("Recharge successful");
+        }
+        else {
+            System.out.println("Phone is locked.");
+        }
+    }
+
+    public float adjustAfterCall(float timeDuration, float costPerMinute) {
+        if (lock == false) {
+            float cost = timeDuration*costPerMinute; 
+            mobileBalance -= cost;
+
+            System.out.println("Cost: " + cost);
+            System.out.println("New balance: " + mobileBalance);
+
+            return cost;
+        }
+        else {
+            System.out.println("Phone is locked.");
+            return 0f;
+        }
+        
+    }
+
+
+
 
 
     
---
+++ OOP1/Lab_Task-2/Mobile/Start.java
@@ -0,0 +1,34 @@
+public class Start {
+    public static void main (String []args) {
+        Mobile m1 = new Mobile("Sadman Sakib", "0171111111", 75, "Android", true);
+        Mobile m2 = new Mobile("Jane Doe", "016111111111", 30, "iOS", true);
+        Mobile m3 = new Mobile("Jon Doe", "015111111111", 100, "iOS", true);
+        Mobile m4 = new Mobile();
+
+        m4.setMobileOwnerName("Jon Snow");
+        m4.setMobileNumber("01312312322");
+        m4.setMobileBalance(45);
+        m4.setMobileOSName("Android");
+        m4.setLock(true);
+
+        m1.showDetails();
+
+        m1.setLock(false);
+        m1.showDetails();
+
+        m2.setLock(false);
+        m2.showDetails();
+
+        m3.setLock(false);
+        m3.showDetails();
+
+        m4.setLock(false);
+        m4.showDetails();
+
+
+        m1.setLock(false);
+        m1.recharge(50);
+        m1.adjustAfterCall(2, 3);
+
+    }
+}
